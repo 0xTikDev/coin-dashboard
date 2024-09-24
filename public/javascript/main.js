90d1eefe858da38dd3a08906cabf2a0b44c06681
TikDev <td.tikdev@gmail.com>
2024-09-24 18:31:16 +0100

    integrate rader chat to track volume and improve ui

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -30,7 +30,8 @@ function stylus(old, current) {
         s: temporal.s.replace("USDT", ""), // then display only underlying asset without USDT
         c: formatPrice(temporal.c), // call formatPrice function to format price decimal places
         P: parseFloat(temporal.P).toFixed(2),
-        v: parseFloat(temporal.v).toFixed(0)
+        v: parseFloat(temporal.v).toFixed(0), //Volumen in quoted asset
+        q: parseFloat(temporal.q).toFixed(0) //Volume in USD
     }));
 
     const tickerBody = document.getElementById("ticker-body");
@@ -57,8 +58,65 @@ usdtPairs.forEach(ticker => {
 });
    
 
-    };
-    
-
 
 // SECTION B: CHART/GRAPH RENDERING ...
+
+// sort according to  volume descending and cut out the first 10 (volume in USD)
+let top10To30 = (usdtPairs.sort((a, b) => {
+    return b.q - a.q;
+})).slice(9, 30);
+let top30To50 = (usdtPairs.sort((a, b) => {
+    return b.q - a.q;
+})).slice(30, 50);
+let leftChart = document.getElementById('chart1').getContext('2d');
+let rightChart = document.getElementById('chart2').getContext('2d');
+let leftBackground = [
+    "#FF5733", "#FFBD33", "#DBFF33", "#75FF33", "#33FF57",
+    "#33FFBD", "#33DBFF", "#3375FF", "#335BFF", "#5733FF",
+    "#BD33FF", "#FF33DB", "#FF33A6", "#FF3375", "#FF6F33",
+    "#FF8D33", "#FFD833", "#D5FF33", "#33FF8D", "#33A6FF"
+];
+let rightBackground = [
+    "#6A1B9A", "#D50000", "#FF6F00", "#FFB300", "#AEEA00",
+    "#00C853", "#00B0FF", "#0091EA", "#304FFE", "#6200EA",
+    "#FF3D00", "#FFD740", "#69F0AE", "#00E676", "#B2FF59",
+    "#FFEA00", "#FFD180", "#FF8A00", "#D500F9", "#FFAB40", "#FF5722"
+];
+leftLabel = 'Volume: top 10 to 20 tickers';
+rightLabel = 'Volume: top 30 to 50 ticker';
+
+function magicChart(pair, htmlTarget, bColor, label) {
+    let volumeChart = new Chart(htmlTarget, {
+        type: 'radar', //bar, horizontalBar, pie, line, doughnut, radar, polarArea
+        data: {
+            labels: pair.map(target => (target.s)),
+            datasets: [{
+                label: label, 
+                data: pair.map(target => (target.q)),
+                backgroundColor: bColor,
+                borderWidth:0.1,
+                borderColor: 'purple',
+            
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top', 
+                    labels: {
+                        boxWidth: 10,
+                        padding: 10,
+                    }
+                }
+            }
+        }
+    });
+    return volumeChart;
+}
+magicChart(top10To30, leftChart, leftBackground, leftLabel);
+magicChart(top30To50, rightChart, rightBackground, rightLabel);
+    
+};
+    
